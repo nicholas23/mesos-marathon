b4a1bb2af34df996609f60273003d9912351fc52
Nicholas Hsu <jnick23@gmail.com>
2016-11-15 17:03:47 +0800

    first commit

---
+++ mesos-marathon.sh
@@ -0,0 +1,9 @@
+#zookeeper
+docker run -d --name=zookeeper garland/zookeeper
+#mesos master
+docker run -d -p 5050:5050 --link zookeeper:zookeeper mesosphere/mesos:1.0.1-2.0.93.ubuntu1404 mesos-master --work_dir=/tmp --zk=zk://zookeeper:2181/mesos --quorum=1
+#mesos slave
+docker run -d --link zookeeper:zookeeper -v /sys/fs/cgroup:/sys/fs/cgroup -v /var/run/docker.sock:/var/run/docker.sock mesosphere/mesos:1.0.1-2.0.93.ubuntu1404 mesos-slave --master=zk://zookeeper:2181/mesos
+docker run -d --link zookeeper:zookeeper -v /sys/fs/cgroup:/sys/fs/cgroup -v /var/run/docker.sock:/var/run/docker.sock mesosphere/mesos:1.0.1-2.0.93.ubuntu1404 mesos-slave --master=zk://zookeeper:2181/mesos
+#marathon
+docker run -d -p 8080:8080 --link zookeeper:zookeeper mesosphere/marathon --master zk://zookeeper:2181/mesos --zk zk://zookeeper:2181/marathon
